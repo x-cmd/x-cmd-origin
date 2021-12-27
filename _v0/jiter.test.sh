3f4578c9ca3cd5072335d1f6e69d9222c87f8295
L <l@x-cmd.com>
2021-12-27 23:43:56 +0800

    Squashed 'mod/awk/' changes from 8491646c..dad996fa
    dad996fa fix(json.awk): jiter null
    005d207e Annotation filter
    f06fbe46 Merge branch 'main' of https://gitee.com/x-bash/awk into issue_I4LAKL_zqk
    58cc1e3f feat: add jiter_exact function
    3ff37c9e add jiter_exact function
    c8b6c79a add jiter_exact function
    
    git-subtree-dir: mod/awk
    git-subtree-split: dad996fa60ba896d0d80cbb90d1279fcb986466b

--- _v0/jiter.test.sh
+++ _v0/jiter.test.sh
@@ -7,28 +7,29 @@ xrc awk
 SSS="$(cat default.awk)$(cat json.awk)"
 
 f1(){
-    awk "$SSS
+    awk "$SSS"'
     BEGIN{
 
     }
     {
-        if (\$0 != \"\") {
-            jiter_(\$0)
-            # print (\$0)
+        if ($0 != "") {
+            # jiter(_, $0)
+            jiter_exact(_, $0, "1.1")
+            # print ($0)
         }
     }
     END{
-        print \"---\" _[S q(1) S q(\"b\") S q(6) ]
-        print(_[ jkey(1, \"b\", 6) ])
-        print jget(_, \"1.1.name\")
-        # print jget(_, \"1.a\")
-        # print jget(_, \".a\")
-        # print jget(_, \".c.a\")
-        # print json_stringify_machine(_, \"1.1.commit\")
+        # print "---" _[S q(1) S q("b") S q(6) ]
+        # print(_[ jkey(1, 2, "commit", "sha") ])
+        # print jget(_, "1.1.name")
+        # print jget(_, "1.a")
+        # print jget(_, ".a")
+        # print jget(_, ".c.a")
+        # print json_stringify_machine(_, "1.1.commit")
         # print json_stringify_compact(_)
-        print json_stringify_format(_, \".\", 4)
+        print json_stringify_format(_, "1.1", 4)
     }
-    "
+    '
 }
 
 f(){

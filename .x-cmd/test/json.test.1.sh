4c41854c968d1e8957d471339171ae0c517e6d4d
L <l@x-cmd.com>
2022-03-25 23:39:02 +0800

    Squashed 'mod/json/' changes from 8cd40096..0b9db823
    0b9db823 ...
    bc15881a ...
    956feb2b ...
    8938aab4 ...
    3706ff22 ...
    9e9b8739 ...
    97006838 ...
    00e5e635 update: testcase
    2cd6b754 update: testcase
    15e9247f update: testcase
    aa6912d4 ...
    6277967b ...
    76fa3979 update: json v0
    87cadb4e ...
    7e38cb54 ...
    92a13397 ...
    2c67d560 ...
    d6a51fdd ...
    4c86f7ca ...
    5b55f463 ...
    b817a00b ...
    0c8e1b4f ...
    cc3b6d5d ...
    20d54d06 ...
    636b01d0 ...
    4e16439c ...
    a0baea67 ...
    b1d29372 ...
    ee4c4436 ...
    91ef7a0a ...
    be95b912 ...
    ec46b993 Merge branch 'main' of gitee.com:x-bash/json
    c33f5540 ...
    d9fc47d3 ...
    582916d2 ...
    d95657dd ...
    ae9d861a !2 【Json】| 修复var函数的定义问题 | 新增了del函数 | 优化了help文档 | deadline:2021-10-26 Merge pull request !2 from fix/issue_I4F9AQ_zqb
    db096cc9 fix(json): remove debug func for main script
    fbe6ae83 fix(json): fix require file of xrc
    c6d47e0f docs(json): add delete func in help doc
    d4b75b51 style(json): update json module highlight color
    5ea975fa fix(json): fix var del shift pop func for json
    0d8ca9d5 docs(json): add pull request file of gitee
    aa4aec66 test(json): add json test file and test cmd
    233295ab -m
    d72a8fb6 fix shift 2
    4132acba fix "$" --- {!}
    c5a32624 ...
    53b8f524 ...
    d7ae8832 ...
    8060a38d ...
    933b9838 fix zsh {!} bug
    1062d6e3 Fix * glob pattern.
    b0bd0668 ...
    118cb9b8 ...
    db95ff84 ...
    2efaeb77 ...
    99c8e73f ...
    48e176a8 ...
    f015bb5c ...
    aba28561 ...
    e6431f51 Merge branch 'master' of gitee.com:x-bash/json
    744a1508 ...
    56ea67e2 v0 subcommand
    7b4cb1bf merge
    032b91a4 fix start
    2157bea2 ...
    cee91073 ...
    7f9f7899 ...
    e939535e ...
    63b1278f ...
    1b855587 ...
    2525e5b9 posix
    5fbe0014 latest
    001ee70b rename
    dcade16b reorganized
    c5ae28ad stash
    c0c85279 Merge branch 'master' of gitee.com:x-bash/json
    22bfd22a add code from std/dict
    37ea69d7 update json.
    8790ff1b add LICENSE.
    b180f8ad ready
    54511764 small adjustment
    52c35e7e Don't eval if awk fail
    55873516 Fix a bug in json_walk.awk
    c18ac44f ready
    366eed3d git.push.sh
    1899b842 Remove code
    e7c70e49 ready
    3ded53ad renamed
    a4170876 Add ticktick test
    708cf4ab Fix a walk_number bug in pseudo string case.
    d2eb9845 bugfix
    3e3767fb fix json_walk regex for string walk
    26bd65e9 json_acc
    cf0dc0e7 Simple acc ready
    68b1ccdf ready
    8578f488 Optimized json_acc
    e1a62cb6 optimize json_acc
    e2147b16 move code
    4476193c Using enum to optimize the performance.
    2400b0ed bugfix
    63a7594c Ready to distribute
    ce52f456 json_walk optimized version
    ef2f7414 json_walk first optimized version
    bcf46af8 New design is ready.
    aa2b5dc1 Every function prepared except pop
    c22849f3 fix space
    881d3bb0 ready
    2290d151 upload latest implementation
    9d03ed94 Fix the format bug
    019e26ca Add append prepend
    ed06f6e6 Add putkv function
    bbb2335b add putkv
    6b8a3bed support {} [] in josn.dict
    ca648ded Using number regex
    a3ebd90e ready
    020e7c0f more functions to come
    cfea73c5 ready
    9098f1f9 More functions to come
    db72f19b add delete
    849dad8d json_walk_string_idx
    2efd4776 stash
    eb6540ea add content generator control
    8b74a346 Add data returning.
    5f77fc9e remove unused callback
    00b542fe Indent optimization
    aaa8f6d7 Add pseudo string support.
    c8fb1f5b stash
    183f9dd8 if code
    deaefbb8 ready
    3263323d Support busybox
    031b32df multiple versions of json implementation
    f374c057 Add a color scheme
    3c8d4b91 Now we add three types of color scheme.
    37a613f6 add color text
    d0edcdc7 stringify
    76d4a23b ready
    785bcae9 add indent space
    8187473f now support multiple types of comments in json
    157e2895 fix
    1bb6fa96 known best
    c8b4a443 small fix
    1994b82c Known be best
    151e5e0a Using iterate to replace match function
    47af1731 Introduce another json_walk_empty() implementation
    ff5324d4 ready
    65f23a94 stash
    febb4e84 Optimize the json parser.
    bf1e6889 remove string manipulation in walker
    6392a4f7 remove some debug
    9603a67e Add log
    b556c294 add json_walk_panic
    
    git-subtree-dir: mod/json
    git-subtree-split: 0b9db823e1f31013bc4d60733c9ba7a3194e1a7d

--- .x-cmd/test/json.test.1.sh
+++ .x-cmd/test/json.test.1.sh
@@ -1,46 +1,34 @@
-. ./v0
+. ./latest
+. ./lib/awk
+. ./lib/generator
 
-res="$(___json_awk_end '
-END {
-    print jget(_, ".1.a")
-    print jget(_, ".1.b")
-}
-' <<A
+___x_cmd_json_var aa <<A
 [
-    { "a": 1, "b": 2 }
-]
-A
-)"
-
-echo "$res"
-
-___json_var a b <<A
-$res
-A
-
-echo "a: $a"
-echo "b: $b"
-
-
-time ___json_awk_get a1=.1.a b1=.1.b <<A
-[
-    { "a": 1, "b": 2 }
+    { "a": [1], "b": 2 }
 ]
 A
 
-echo "a1: $a1
-b1: $b1"
+___x_cmd_json_push "aa.[0].a" "2"
+___x_cmd_json_push aa '{"name": ["Edwin"],"age": 31,"gender": "male","score": {"math": 100}}'
+___x_cmd_json_push aa "ccc"
 
-time ALREADY_TOKENIZED=1 ___json_awk_get a1=.1.a b1=.1.b <<A
-[
-{
-"a"
-:
-1
-,
-"b"
-:
-2
-}
-]
-A
+echo "aa: $aa"
+___x_cmd_json_var bb "{a:1}"
+___x_cmd_json_put bb.b "{b1:bbb,b2:[1,2,22,33]}"
+
+x json color bb
+
+___x_cmd_json_pop bb.b.b2           # del end
+___x_cmd_json_push bb.b.b2 33       # add end and no printf
+___x_cmd_json_shift bb.b.b2         # del head
+___x_cmd_json_prepend bb.b.b2 11    # add head
+___x_cmd_json_del "bb.b.b2.[1]"
+
+___x_cmd_json_values bb.b.b2
+___x_cmd_json_keys bb.b
+___x_cmd_json_length bb.b
+
+___x_cmd_json_query "bb.b.b2.[1]"
+___x_cmd_json_query bb.b
+
+# ___x_cmd_json_unescape ___x_cmd_json_query bb.b
